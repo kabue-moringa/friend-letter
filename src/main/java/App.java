d9cfb0e746d854ed2b0d04f7a22d104305049daf
kabue-moringa <joseph.wangari@student.moringaschool.com>
2021-06-02 00:43:19 +0300

    add changes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,41 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
 import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
-        get("/", (request, response) ->
+        get("/", (request, response) -> {
 
-        "<!DOCTYPE html>" +
-                "<html>" +
-                "<head>" +
-                "<title>Hello Friend!</title>" +
-                "<link rel='stylesheet' + href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>" +
-                "</head>" +
-                "<body>" +
-                "<p><a href='/favorite_photos' >P.S. Check out my favorite travel photos here.</a></p>" +
-                "<h1>Hello From Afar</h1>" +
-                "<p>Dear Friend,</p>" +
-                "<p>How are you? I hope that you are having a nice weekend. I'm vacationing in the Iceland while I learn programming! </p>" +
-                "<p>Friend, you would not believe how cold it is here. I should have gone to Hawaii instead.</p>" +
-                "<p>But I like programming a lot, so I've got that going for me. </p>" +
-                "<p>Looking forward to seeing you soon. I'll bring you back a souvenir. </p>" +
-                "<p>Cheers,</p>" +
-                "<p>Travel Enthusiast Jane</p>" +
-                "</body>" +
-
-                "</html>"
-    );
-
-        get("/favorite_photos", (request, response) ->
-                "<!DOCTYPE html>" +
-                        "<html>" +
-                        "<head>" +
-                        "<title>Hello Friend!</title>" +
-                        "<link rel='stylesheet'  href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>" +
-                        "</head>" +
-                        "<body>" +
-                        "<h1>Favorite Traveling Photos</h1>" +
-                        "<ul>" + "<li><img src='/image/images.jpg' alt='A photo of a mountain range.'/></li>" + "<li><img src='/image/download.jpg' alt='A photo of a swamp.'/></li>" + "</ul>" + "</body>" + "</html>"
-        );
+            return new ModelAndView(new HashMap(), "hello.hbs");
+        }, new HandlebarsTemplateEngine());
+        get("/favorite_photos", (request, response) -> {
+            return new ModelAndView(new HashMap(), "favorite_photos.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }

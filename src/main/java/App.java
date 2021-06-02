860cf8d7d360ff7d74d69427033e13d1bc593e4c
kabue-moringa <joseph.wangari@student.moringaschool.com>
2021-06-02 15:04:55 +0300

    add the changes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,18 +1,34 @@
-import java.util.HashMap;
-import java.util.Map;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
-import static spark.Spark.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.get;
+import static spark.Spark.staticFileLocation;
 public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
         get("/", (request, response) -> {
-
-            return new ModelAndView(new HashMap(), "hello.hbs");
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "hello.hbs");
         }, new HandlebarsTemplateEngine());
         get("/favorite_photos", (request, response) -> {
-            return new ModelAndView(new HashMap(), "favorite_photos.hbs");
+            Map<String,Object> model =new HashMap<String, Object>();
+            return new ModelAndView(model, "favorite_photos.hbs");
+        }, new HandlebarsTemplateEngine());
+        get("/form", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "form.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/greeting_card", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            String recipient = request.queryParams("recipient");
+            String sender = request.queryParams("sender");
+            model.put("recipient", recipient);
+            model.put("sender", sender);
+            return new ModelAndView(model, "greeting_card.hbs");
         }, new HandlebarsTemplateEngine());
     }
 }
